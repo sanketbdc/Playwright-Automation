1b9017255808f8bde98c8334983ab8c4528c2a34
ShivaKanchiBDC <shiva@bombaydc.com>
2026-04-01 18:45:43 +0530

    added email recipient

---
+++ sendReport.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+async function sendMail() {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: 'sanket@bombaydc.com',
+    subject: 'Weekly Playwright Execution Report',
+    text: 'Please find attached Playwright HTML report.',
+    attachments: [
+      {
+        filename: 'playwright-report.zip',
+        path: './playwright-report.zip'
+      }
+    ]
+  });
+
+  console.log('Email sent successfully');
+}
+
+sendMail();

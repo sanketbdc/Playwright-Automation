7d36e8e5bc1595381c04f90fac38b7f8fbe14249
ShivaKanchiBDC <shiva@bombaydc.com>
2026-04-15 16:30:03 +0530

    changes are made in CI CD workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,13 +11,16 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : undefined,
 
   reporter: [['html'], ['list'], ['./utils/excelReporter.js']],
 
   use: {
+    headless: true,
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
   },
 
   projects: [
